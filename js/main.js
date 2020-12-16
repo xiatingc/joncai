8d3f59913748f1cbb1b33d1cf5d045772106d8d8
xman <41560782+xiatingc@users.noreply.github.com>
2020-12-16 21:21:14 +1100

    up

--- js/main.js
+++ js/main.js
@@ -326,7 +326,7 @@ $(function(){
         if (animation) {
     
           var text = $('#home .typer-title'),
-              textOne = "i'm educational technologist",
+              textOne = "i'm a web designer and app tester",
               textTwo = "let's work together",
               textThree = "i can create awesome stuff";
     
