df5a874b6a5a36491f7e906e75070881b192b27d
xman <41560782+xiatingc@users.noreply.github.com>
2021-02-03 12:19:57 +1100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -326,7 +326,7 @@ $(function(){
         if (animation) {
     
           var text = $('#home .typer-title'),
-              textOne = "i'm a web designer and app developer",
+              textOne = "i'm a Business Analyst and junior Product Manager",
               textTwo = "let's work together",
               textThree = "i can create awesome stuff";
     
