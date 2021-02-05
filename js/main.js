36f0d68323ab428924f8567ece46eba04aaeff49
xman <41560782+xiatingc@users.noreply.github.com>
2021-02-05 15:32:05 +1100

    up

--- js/main.js
+++ js/main.js
@@ -326,7 +326,7 @@ $(function(){
         if (animation) {
     
           var text = $('#home .typer-title'),
-              textOne = "i'm a Business Analyst and junior Product Manager",
+              textOne = "i'm a junior Business Analyst and Product Manager",
               textTwo = "let's work together",
               textThree = "i can create awesome stuff";
     
