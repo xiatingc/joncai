69db5fce359a7bbaee80bbfee893147fb1529900
xman <41560782+xiatingc@users.noreply.github.com>
2021-02-17 20:26:08 +1100

    up

--- js/main.js
+++ js/main.js
@@ -326,7 +326,7 @@ $(function(){
         if (animation) {
     
           var text = $('#home .typer-title'),
-              textOne = "a digital geek, a creative thinker",
+              textOne = "a digital geek and a creative thinker",
               textTwo = "let's work together",
               textThree = "i can create awesome stuff";
             
